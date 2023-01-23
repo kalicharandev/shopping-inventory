6497eedd4a267177ff0721b96fd08992f822dba0
pradheepap <pradheepa.p@gmail.com>
2023-01-23 21:11:37 +0800

    send props to product list

--- src/ProductsList.js
+++ src/ProductsList.js
@@ -1,27 +1,24 @@
 import './App.css';
-function ProductsList(){
 
-  const Products = [
-   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-   {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-   {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-   {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-   {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-   {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
- ];
- let productsList = "";
- for(let i = 0; i < Products.length; i++) {
- 
- productsList += `<ul><li>${Products[i].name} price RS : ${Products[i].price}</li></ul>`
- 
-}
+
+function ProductsList({products}){
+
+  const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.name}
+    </li>
+  );  
+
+
 return(
 <div className='product-list'>
-    {productsList}
+  
+  <ul>{listItems}</ul>
+  
 </div>
 
 )
+};
 
-}
 
 export default ProductsList;
